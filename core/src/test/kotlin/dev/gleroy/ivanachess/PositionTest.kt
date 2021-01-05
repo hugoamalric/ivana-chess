21ae453e38aab65bef4a9407b02edfa0d25ee951
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2021-01-05 16:22:49 +0100

    :white_check_mark: core: add Position.toString() tests

--- core/src/test/kotlin/dev/gleroy/ivanachess/PositionTest.kt
+++ core/src/test/kotlin/dev/gleroy/ivanachess/PositionTest.kt
@@ -3,6 +3,7 @@
 package dev.gleroy.ivanachess
 
 import io.kotlintest.matchers.throwable.shouldHaveMessage
+import io.kotlintest.shouldBe
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -35,6 +36,19 @@ internal class PositionTest {
                 instantiate()
             }
             exception shouldHaveMessage "$propertyName must be between ${Position.Min} and ${Position.Max}"
+        }
+    }
+
+    @Nested
+    inner class toString {
+        @Test
+        fun `should return A1`() {
+            Position(1, 1).toString() shouldBe "A1"
+        }
+
+        @Test
+        fun `should return B3`() {
+            Position(2, 3).toString() shouldBe "B3"
         }
     }
 }
