49c4a5640c7bd66e434c97a39c9e691882f35041
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2021-02-10 20:45:12 +0100

    :memo: upgrade to 0.2.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ allprojects {
     } else {
         "$rootGroup.${projectDir.parentFile.name}"
     }
-    version = "0.1.0"
+    version = "0.2.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
